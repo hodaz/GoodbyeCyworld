0a32a8e00d4aadb479d7819a2cb62e8e08c24f95
hodaz <hano2@nate.com>
2016-03-10 16:04:33 +0900

    recreate defines

--- app/src/main/java/com/hodaz/goodbyecyword/common/defines.java
+++ app/src/main/java/com/hodaz/goodbyecyword/common/defines.java
@@ -3,5 +3,7 @@ package com.hodaz.goodbyecyword.common;
 /**
  * Created by hodaz on 2016. 3. 10..
  */
-public class defines {
+public class Defines {
+    public static final String URL_GET_FOLDER = "http://cy.cyworld.com/home/%1$s/menu/?type=folder";
+    public static final String URL_GET_CONTENT_LIST = "http://cy.cyworld.com/home/%1$s/postlist?startdate=&enddate=&folderid=%2$s&tagname=&_=1457400136714";
 }
